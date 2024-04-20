4ba0b4de69b437743df2c14ad1d985d3ba266b74
Pyae Sone Soe <pyaesone.ps15@gmail.com>
2024-04-20 11:32:25 +0630

    merge test

---
+++ Final Project/HotelManagementSystem/HotelManagementSystem/Views/Payment/UCPayment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelManagementSystem.Views.Payment
+{
+    public partial class UCPayment : UserControl
+    {
+        public UCPayment()
+        {
+            InitializeComponent();
+        }
+    }
+}
