b9cf728860c057dd89add73613f99e801801223b
Pyae Sone Soe <pyaesone.ps15@gmail.com>
2024-04-24 08:59:04 +0630

    design update

--- Final Project/HotelManagementSystem/HotelManagementSystem/Views/Menu/FrmMenu.Designer.cs
+++ Final Project/HotelManagementSystem/HotelManagementSystem/Views/Menu/FrmMenu.Designer.cs
@@ -244,6 +244,7 @@ namespace HotelManagementSystem.Views.Menu
             this.ClientSize = new System.Drawing.Size(1105, 775);
             this.Controls.Add(this.pnMain);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
