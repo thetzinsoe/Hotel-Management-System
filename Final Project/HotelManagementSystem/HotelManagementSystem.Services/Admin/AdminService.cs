8b1552d3d30e5e649341f5e7bf7988bb427a17de
nearlyshippe <136670434+nearlyshippe@users.noreply.github.com>
2024-04-24 09:37:59 +0630

    adminupdate

--- Final Project/HotelManagementSystem/HotelManagementSystem.Services/Admin/AdminService.cs
+++ Final Project/HotelManagementSystem/HotelManagementSystem.Services/Admin/AdminService.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelManagementSystem.DAO.Admin;
+using HotelManagementSystem.Entities.Admin;
 
 namespace HotelManagementSystem.Services.Admin
 {
-    internal class AdminService
+    public class AdminService
     {
+        private AdminDao adminDao = new AdminDao();
+
+        // Get All Admins
+        public DataTable GetAllAdmins()
+        {
+            return adminDao.GetAllAdmins();
+        }
+
+        // Get Admin by Id
+        public DataTable GetAdminById(int adminId)
+        {
+            return adminDao.GetAdminById(adminId);
+        }
+        //get admin by username
+        public DataTable GetAdminByUsername(string username)
+        {
+            return adminDao.GetAdminByUsername(username);
+        }
+        // Insert Admin
+        public bool InsertAdmin(AdminEntity adminEntity)
+        {
+            return adminDao.InsertAdmin(adminEntity);
+        }
+
+        // Update Admin
+        public bool UpdateAdmin(AdminEntity adminEntity)
+        {
+            return adminDao.UpdateAdmin(adminEntity);
+        }
+
+        // Delete Admin
+        public bool DeleteAdmin(int adminId)
+        {
+            return adminDao.DeleteAdmin(adminId);
+        }
     }
 }
